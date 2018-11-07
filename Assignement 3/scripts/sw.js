8ed461618f6f10068034ff018cda93e74be03ccc
Smamkele <smsmamkie@gmail.com>
2018-11-07 16:50:25 +0200

    updated images

--- Assignement 3/scripts/sw.js
+++ Assignement 3/scripts/sw.js
@@ -1,32 +1,22 @@
 self.importScripts('scripts/main.js');
 
 // Files to cache
-var cacheName = 'js13kPWA-v1';
+var cacheName = 'assignement3';
 var appShellFiles = [
-  '/pwa-examples/js13kpwa/',
-  '/pwa-examples/js13kpwa/index.html',
-  '/pwa-examples/js13kpwa/app.js',
-  '/pwa-examples/js13kpwa/style.css',
-  '/pwa-examples/js13kpwa/fonts/graduate.eot',
-  '/pwa-examples/js13kpwa/fonts/graduate.ttf',
-  '/pwa-examples/js13kpwa/fonts/graduate.woff',
-  '/pwa-examples/js13kpwa/favicon.ico',
-  '/pwa-examples/js13kpwa/img/js13kgames.png',
-  '/pwa-examples/js13kpwa/img/bg.png',
-  '/pwa-examples/js13kpwa/icons/icon-32.png',
-  '/pwa-examples/js13kpwa/icons/icon-64.png',
-  '/pwa-examples/js13kpwa/icons/icon-96.png',
-  '/pwa-examples/js13kpwa/icons/icon-128.png',
-  '/pwa-examples/js13kpwa/icons/icon-168.png',
-  '/pwa-examples/js13kpwa/icons/icon-192.png',
-  '/pwa-examples/js13kpwa/icons/icon-256.png',
-  '/pwa-examples/js13kpwa/icons/icon-512.png'
+  '/Find-My-Transport/assignement3/',
+  '/Find-My-Transport/assignement3/index.html',
+  '/Find-My-Transport/assignement3/bulma.min.css',
+  '/Find-My-Transport/assignement3/scripts/main.js',
+  '/Find-My-Transport/assignement3/scripts/sw.js',
+  '/Find-My-Transport/assignement3/scripts/manifest.json',
+  '/Find-My-Transport/assignement3/images/big.png',
+  '/Find-My-Transport/assignement3/images/images512.jpeg',
+  '/Find-My-Transport/assignement3/images/images192.jpeg',
+  '/Find-My-Transport/assignement3/images/taxi.jpeg',
+  
 ];
-var gamesImages = [];
-for(var i=0; i<games.length; i++) {
-  gamesImages.push('data/img/'+games[i].slug+'.jpg');
-}
-var contentToCache = appShellFiles.concat(gamesImages);
+
+var contentToCache = appShellFiles;
 
 // Installing Service Worker
 self.addEventListener('install', function(e) {
